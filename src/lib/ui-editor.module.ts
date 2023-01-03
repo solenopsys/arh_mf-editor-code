39faf1e84d65f147b557f795c869d0b0a4fa3fc6
AlexStorm <info@alexstorm.ru>
2023-01-03 13:07:54 +0530

    fixed and change docks

--- src/lib/ui-editor.module.ts
+++ src/lib/ui-editor.module.ts
@@ -4,17 +4,37 @@ import {RouterModule} from '@angular/router';
 
 import {UIControlsModule} from '@solenopsys/uimatrix-controls';
 import {DeclaredService} from "@solenopsys/uimatrix-utils";
+import {CodeAreaComponent} from "./code-area/code-area.component";
+import {FormsModule} from "@angular/forms";
+import {MonacoEditorModule, NgxMonacoEditorConfig} from "@tots/ngx-monaco-editor-v2";
 
 const components:any = [
-
+  CodeAreaComponent
 ];
+export function onMonacoLoad() {
+  console.log('MONACO START');
+ // console.log('MONACO OK', (window as any).monaco);
+  // (window as any).monaco.languages.typescript.typescriptDefaults.addExtraLib('export function drawPins() {\n' +
+  //     '    console.log(\'OK\');\n' +
+  //     '}\n');
+}
+
+const monacoConfig: NgxMonacoEditorConfig = {
+  baseUrl: 'assets',
+  defaultOptions: {scrollBeyondLastLine: false},
+  onMonacoLoad
+};
+
 
 @NgModule({
   declarations: components,
   imports: [
+
     CommonModule,
     RouterModule,
     UIControlsModule,
+    FormsModule,
+    MonacoEditorModule.forRoot(monacoConfig),
   ],
   providers: [],
   exports: [
